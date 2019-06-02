7f0e19b737724cccd540a96e7c174393e49abaac
stkzk3110 <fisherman0825dd@gmail.com>
2019-06-02 21:01:46 +0900

    Added file upload function and CSV file import / export function

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+
+    has_one_attached :image
+
     validates :name, presence: true, length: { maximum: 30 }
     validate :validate_name_not_includeing_comma
 
@@ -6,7 +9,28 @@ class Task < ApplicationRecord
 
     scope :recent, -> { order(created_at: :desc) }
 
-    private
+    def self.csv_attributes
+        ["name", "description", "created_at", "updated_at"]
+    end
+
+    def self.generate_csv
+        CSV.generate(headers: true) do |csv|
+            csv << csv_attributes
+            all.each do |task|
+                csv << csv_attributes.map{|attr| task.send(attr) }
+            end
+        end
+    end
+
+    def self.import(file)
+        CSV.foreach(file.path, headers: true) do |row|
+            task = new
+            task.attributes = row.to_hash.slice(*csv_attributes)
+            task.save!
+        end
+    end
+
+private
 
     def validate_name_not_includeing_comma
         errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
